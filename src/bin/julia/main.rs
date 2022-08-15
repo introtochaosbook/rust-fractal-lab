752bf6ff68636c82b023a799f58415f773bbbd58
Chris Laplante <mostthingsweb@gmail.com>
2022-08-14 21:53:50 -0400

    first crack at julia set, not working yet

---
+++ src/bin/julia/main.rs
@@ -0,0 +1,129 @@
+use glium::glutin::dpi::LogicalSize;
+use glium::glutin::event::{DeviceEvent, Event, WindowEvent};
+use glium::glutin::event_loop::{ControlFlow, EventLoop};
+use glium::glutin::window::WindowBuilder;
+use glium::glutin::ContextBuilder;
+use glium::index::{NoIndices, PrimitiveType};
+use glium::uniforms::{UniformValue, Uniforms};
+use glium::{Display, Program, Surface, VertexBuffer};
+use rust_fractal_lab::vertex::Vertex;
+
+#[derive(Debug)]
+struct DrawParams {
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+
+    width: f64,
+    height: f64,
+    max_colors: u32,
+}
+
+impl DrawParams {
+    fn new(dims: (u32, u32)) -> DrawParams {
+        DrawParams {
+            x_min: -2.0,
+            x_max: 1.0,
+            y_min: -1.0,
+            y_max: 1.0,
+            width: dims.0 as f64,
+            height: dims.1 as f64,
+            max_colors: 100,
+        }
+    }
+}
+
+impl Uniforms for DrawParams {
+    fn visit_values<'a, F: FnMut(&str, UniformValue<'a>)>(&'a self, mut f: F) {
+        f("xMin", UniformValue::Double(self.x_min));
+        f("xMax", UniformValue::Double(self.x_max));
+        f("yMin", UniformValue::Double(self.y_min));
+        f("yMax", UniformValue::Double(self.y_max));
+        f("width", UniformValue::Double(self.width));
+        f("height", UniformValue::Double(self.height));
+        f("maxColors", UniformValue::UnsignedInt(self.max_colors));
+    }
+}
+
+fn main() {
+    let mut event_loop = EventLoop::new();
+
+    let wb = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world");
+
+    let cb = ContextBuilder::new();
+
+    let display = Display::new(wb, cb, &event_loop).unwrap();
+
+    let vertices = [
+        Vertex {
+            position: [1.0, -1.0],
+        },
+        Vertex {
+            position: [-1.0, 1.0],
+        },
+        Vertex {
+            position: [-1.0, -1.0],
+        },
+        Vertex {
+            position: [1.0, 1.0],
+        },
+        Vertex {
+            position: [1.0, -1.0],
+        },
+        Vertex {
+            position: [-1.0, 1.0],
+        },
+    ];
+
+    let vertex_buffer = VertexBuffer::new(&display, &vertices).unwrap();
+    let indices = NoIndices(PrimitiveType::TrianglesList);
+
+    let program = Program::from_source(
+        &display,
+        r##"#version 140
+in vec2 position;
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0);
+}
+"##,
+        include_str!("shaders/fragment.glsl"),
+        None,
+    )
+        .unwrap();
+
+    let mut draw_params = DrawParams::new(display.get_framebuffer_dimensions());
+
+    event_loop.run(move |ev, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match ev {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            Event::DeviceEvent {
+                event: DeviceEvent::MouseMotion { .. } | DeviceEvent::Motion { .. },
+                ..
+            } => return,
+            _ => (),
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &draw_params,
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
+}
