a15ae89d5b98c45bc6e118475d8d7800d4953076
Chris Laplante <mostthingsweb@gmail.com>
2022-08-14 21:29:29 -0400

    use polynomial approx of colormap

--- src/bin/mandelbrot/shaders/fragment.glsl
+++ src/bin/mandelbrot/shaders/fragment.glsl
@@ -13,11 +13,17 @@ uniform double width;
 
 uniform uint max_colors;
 
-layout(std140) uniform Block {
-    uint colors_r[1024];
-    uint colors_g[1024];
-    uint colors_b[1024];
-};
+vec3 color_map(float t) {
+    const vec3 c0 = vec3(0.0002189403691192265, 0.001651004631001012, -0.01948089843709184);
+    const vec3 c1 = vec3(0.1065134194856116, 0.5639564367884091, 3.932712388889277);
+    const vec3 c2 = vec3(11.60249308247187, -3.972853965665698, -15.9423941062914);
+    const vec3 c3 = vec3(-41.70399613139459, 17.43639888205313, 44.35414519872813);
+    const vec3 c4 = vec3(77.162935699427, -33.40235894210092, -81.80730925738993);
+    const vec3 c5 = vec3(-71.31942824499214, 32.62606426397723, 73.20951985803202);
+    const vec3 c6 = vec3(25.13112622477341, -12.24266895238567, -23.07032500287172);
+
+    return c0+t*(c1+t*(c2+t*(c3+t*(c4+t*(c5+t*c6)))));
+}
 
 vec2 complex_square(vec2 z) {
     return vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y);
@@ -79,6 +85,7 @@ void main() {
     if (mag < escape) {
         color = vec4(0, 0, 0, 1);
     } else {
-        color = vec4(float(colors_r[i]) / 255.0, float(colors_g[i]) / 255.0, float(colors_b[i]) / 255.0, 1);
+        vec3 s = color_map(float(i) / float(max_colors));
+        color = vec4(s.xyz, 1);
     }
 }
