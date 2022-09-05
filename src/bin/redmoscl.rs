2faeb36b17d437880ea97b40b9eb2c65b955156c
Chris Laplante <chris@laplante.io>
2022-09-05 17:16:40 -0400

    supposed to be mist, not clouds...

--- src/bin/redmoscl.rs
+++ src/bin/redmoscl.rs
@@ -24,11 +24,6 @@ fn main() {
         [0.33, 0.0, 0.0, 0.33, inc / 2.0, inc, 0.125],
     ];
 
-    // Draw clouds
-    program.sample(&cloud_d, [99.0 / 255.0, 99.0 / 255.0, 99.0 / 255.0, 1.0], 30000);
-    // Place the clouds roughly in the upper third of the screen
-    program.normalize_points_to_ranges(-1.0, 1.0, 0.6, 1.0);
-
     // Draw the redwoods themselves
     let inc = 6.0;
     let redwood_d: Array<f32, Ix2> = array![
@@ -49,6 +44,11 @@ fn main() {
     draw_forest(&mut program, &mut rng, 75);
     // Keep the trees to the bottom half of the screen
     program.normalize_points_to_ranges(-1.0, 1.0, -1.0, 0.5);
+
+    // Draw mist
+    program.sample(&cloud_d, [129.0 / 255.0, 129.0 / 255.0, 129.0 / 255.0, 1.0], 30000);
+    // Place the mist in upper half of screen
+    program.normalize_points_to_ranges(-1.0, 1.0, 0.5, 1.0);
 
     program.run(Some(1.5));
 }
