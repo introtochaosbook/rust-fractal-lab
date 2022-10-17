3a53343d9c6af8aebb40eb21a152b36cfe113f1b
Chris Laplante <chris@laplante.io>
2022-10-17 00:03:06 -0400

    add Rule trait

--- src/bin/1d_life.rs
+++ src/bin/1d_life.rs
@@ -1,5 +1,3 @@
-extern crate core;
-
 use std::fmt::{Display, Formatter, Write};
 
 use bitvec::vec::BitVec;
@@ -9,16 +7,32 @@ use regex::Regex;
 
 const STAR_CHAR: char = '*';
 
-#[derive(Parser)]
-struct Args {
-    input: String,
-}
-
 // A state is just a vector of bits (true or false, 1 or 0)
 struct State(BitVec);
 
+/// Represents a rule to be applied to the state
+trait Rule {
+    fn apply(a_neg1: bool, a_0: bool, a_1: bool) -> bool;
+}
+
+struct Rule1;
+impl Rule for Rule1 {
+    fn apply(a_neg1: bool, a_0: bool, a_1: bool) -> bool {
+        (a_neg1 && !a_0 && !a_1) || (!a_neg1 && a_1) || (a_0 && a_1)
+    }
+}
+
+struct Rule2;
+impl Rule for Rule2 {
+    fn apply(a_neg1: bool, a_0: bool, a_1: bool) -> bool {
+        (a_neg1 && !a_0 && !a_1) || (!a_neg1 && a_1)
+    }
+}
+
+
 impl State {
-    /// Parse state from input string containing stars, spaces, and/or repeat directives
+    /// Parse state from input string containing stars, spaces, and/or repeat directives.
+    /// This portion of the code is a bit complicated because of the handling for repeat directives.
     pub fn parse<T: AsRef<str>>(input: T) -> Self {
         let input = input.as_ref();
         let mut rng = rand::thread_rng();
@@ -26,9 +40,11 @@ impl State {
         let regex = Regex::new(r"(?P<count>\d+)\[(?P<char>[ *?])]|(?P<single>[ *?])").unwrap();
 
         let mut ret = BitVec::new();
-        let mut start = 0;
 
+        // Keep track of the start of the last match to ensure we consume all of the input
+        let mut start = 0;
         for m in regex.captures_iter(input) {
+            // Handle repeat directive
             if let Some(count) = m.name("count") {
                 if count.start() != start {
                     eprintln!("{} != {}", count.start(), start);
@@ -37,17 +53,22 @@ impl State {
 
                 let repetition_count: usize = count.as_str().parse().unwrap();
                 let char = m.name("char").unwrap();
+
                 // Account for closing square bracket
                 start = char.end() + 1;
+
                 let char = char.as_str();
                 assert_eq!(char.len(), 1);
                 let repetition_char = char.chars().nth(0).unwrap();
+                // This needs to be a closure, otherwise using '?' would result in repeating the
+                // same character, which isn't very random.
                 let repeater = || match repetition_char {
                     STAR_CHAR => true,
                     ' ' => false,
                     '?' => rng.gen_bool(0.5),
                     c => panic!("unexpected char '{}' in repetition '{}[{}]'", c, repetition_count, c),
                 };
+
                 ret.extend(std::iter::repeat_with(repeater).take(repetition_count));
             } else if let Some(single) = m.name("single") {
                 if single.start() != start {
@@ -55,6 +76,7 @@ impl State {
                 }
 
                 start = single.end();
+
                 let c = single.as_str().chars().nth(0).unwrap();
                 match c {
                     STAR_CHAR => ret.push(true),
@@ -74,24 +96,22 @@ impl State {
         Self(ret)
     }
 
-    //
-    fn next(&self) -> State {
+    /// Compute next state given `R`, a rule type
+    fn next<R: Rule>(&self) -> State {
         let mut ret = BitVec::repeat(false, self.0.len());
 
+        // The `Windows` iterator is very helpful here - it saves us from having to manually deal
+        // with boundary conditions.
         for (i, slice) in self.0.windows(3).enumerate() {
-            let a_neg1 = slice[0];
-            let a_0 = slice[1];
-            let a_1 = slice[2];
-
-            ret.set(
-                i + 1,
-                (a_neg1 && !a_0 && !a_1) || (!a_neg1 && a_1) || (a_0 && a_1),
-            );
+            // Apply the rule
+            ret.set(i + 1, R::apply(slice[0], slice[1], slice[2]));
         }
 
         State(ret)
     }
 }
+
+
 
 impl Display for State {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -107,13 +127,18 @@ impl Display for State {
     }
 }
 
+#[derive(Parser)]
+struct Args {
+    input: String,
+}
+
 fn main() {
     let args = Args::parse();
 
     let mut input = State::parse(args.input);
     eprintln!("{}", input);
-    for _ in 0..25 {
-        input = input.next();
+    for _ in 0..250 {
+        input = input.next::<Rule2>();
         eprintln!("{}", input);
     }
 }
