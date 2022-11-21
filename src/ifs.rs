a7abda19f001d2a3eb873d6b7595d697acd09e03
Chris Laplante <chris@laplante.io>
2022-11-20 19:49:42 -0500

    remove unused method

--- src/ifs.rs
+++ src/ifs.rs
@@ -144,35 +144,6 @@ impl IfsProgram {
         self.sample_affine(d, color, iters, 1.0, 0.0, 0.0);
     }
 
-    pub fn draw_forest(&mut self, rng: &mut ThreadRng, count: u32) {
-        let d: Array<f32, Ix2> = array![
-            [0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.05],
-            [0.42, -0.42, 0.42, 0.42, 0.0, 0.2, 0.40],
-            [0.42, 0.42, -0.42, 0.42, 0.0, 0.2, 0.40],
-            [0.1, 0.0, 0.0, 0.1, 0.0, 0.2, 0.15],
-        ];
-
-        for _ in 0..count {
-            let shift_x = rng.gen_range(-0.5..0.5);
-            let shift_y = rng.gen_range(-0.5..0.5);
-            let scale = rng.gen_range(1.0..10.0);
-
-            let color = {
-                match rng.gen_range(0..=9) {
-                    // Most trees are green
-                    0..=7 => [0.0, 0.39, 0.0, 1.0],
-                    // Some trees are yellow
-                    8 => [0.8, 0.95, 0.0, 1.0],
-                    // Some trees are dead (brown)
-                    9 => [0.64, 0.16, 0.16, 1.0],
-                    _ => unreachable!(),
-                }
-            };
-
-            self.sample_affine(&d, color, 2000, scale, shift_x, shift_y);
-        }
-    }
-
     pub fn run(&mut self, point_size: Option<f32>) {
         let event_loop = EventLoop::new();
 
